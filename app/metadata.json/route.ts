1c7682aeb311e4aaa0500d166ef68b0a3e7815f3
bujikanna <bujikanna99@gmail.com>
2025-07-12 13:12:58 +0530

    Update route.ts

--- app/metadata.json/route.ts
+++ app/metadata.json/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({
     name: "GASIO Token List",
-    logoURI: "https://i.ibb.co/BVfqyL4Q/GASIo.jpg",
+    logoURI: "https://gasiotoken.vercel.app/gasio.jpg",
     keywords: ["gas", "stablecoin", "crypto", "trc20", "gasio"],
     timestamp: "2025-07-11T00:00:00+00:00",
     version: { major: 1, minor: 0, patch: 0 },
@@ -14,7 +14,7 @@ export async function GET() {
         symbol: "GASIO",
         name: "Gasio",
         decimals: 6,
-        logoURI: "https://i.ibb.co/BVfqyL4Q/GASIo.jpg",
+        logoURI: "https://gasiotoken.vercel.app/gasio.jpg",
         tags: ["utility", "stablecoin", "trc20"],
         extensions: {
           homepage: "https://gasiotoken.vercel.app/",
